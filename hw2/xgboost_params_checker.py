8e0084005b7ad785d5828bfff704b799f3bf872b
Alex Prozorov <alexprozoroff@gmail.com>
2018-03-12 00:16:28 +0300

    hw2 updates

--- hw2/xgboost_params_checker.py
+++ hw2/xgboost_params_checker.py
@@ -18,7 +18,7 @@ def signal_handler(signum, frame):
 
 
 class Checker(object):
-    def __init__(self, data_path=SCRIPT_DIR + '/../seminar02/HR.csv'):
+    def __init__(self, data_path=SCRIPT_DIR + '/HR.csv'):
         df = pandas.read_csv(data_path)
         target = 'left'
         features = [c for c in df if c != target]
@@ -26,21 +26,17 @@ class Checker(object):
         self.data = np.array(df[features])
 
     def check(self, params_path):
-        try:
-            with open(params_path, 'r') as f:
-                params = json.load(f)
-                signal.signal(signal.SIGALRM, signal_handler)
-                # Time limit на эту задачу 2 минуты
-                signal.alarm(120)
-                estimator = xgb.XGBClassifier(**params)
-                score = np.mean(cross_val_score(
-                    estimator, self.data, self.target,
-                    scoring='accuracy', 
-                    cv=3
-                ))
-        except:
-            print("Unexpected error:", sys.exc_info()[0])
-            score = None
+        with open(params_path, 'r') as f:
+            params = json.load(f)
+            #signal.signal(signal.SIGALRM, signal_handler)
+            # Time limit на эту задачу 2 минуты
+            #signal.alarm(120)
+            estimator = xgb.XGBClassifier(**params)
+            score = np.mean(cross_val_score(
+                estimator, self.data, self.target,
+                scoring='accuracy', 
+                cv=3
+            ))
         
         return score
 
